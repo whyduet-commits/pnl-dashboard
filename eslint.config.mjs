c909c53a16e836c482c884a1d13b16bd540a122b
황정우 <whyduet@gmail.com>
2026-05-20 11:50:44 +0900

    fix eslint and typescript build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,1 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-
-const eslintConfig = defineConfig([
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default [];
